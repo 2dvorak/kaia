d25649742d43576fe0bf5855d49fc4aea395ab4e
Sawyer <18228063+2dvorak@users.noreply.github.com>
2025-07-18 14:21:47 +0900

    Add test case for bad block #571906

--- storage/statedb/flat_trie_test.go
+++ storage/statedb/flat_trie_test.go
@@ -799,3 +799,212 @@ func TestFlatTrieUpdateStorage(t *testing.T) {
 
 	t.Fail()
 }
+
+func TestFlatTrieStorage571906(t *testing.T) {
+	dbm := newEmptyDBManager()
+	defer dbm.Close()
+
+	addr := common.HexToAddress("0xfb390fe9eacaaf75d88bc8ce82038219da25a0ba")
+
+	/*
+		From archive node itertrie
+		./build/bin/ken --datadir data itertrie 0xf1ef081ee354c3d1ff6ec6188c3dd3c181553fb049b2807d3bf9a22d7dbd8487
+
+		// keccak(0000000000000000000000000000000000000000000000000000000000000005)
+		key: 036b6384b5eca791c62761152d0c79bb0604c104a5fb6f4eb0703f3154bb3db0, value: 06
+		// keccak(697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206512)
+		key: 0e3a43cacfe637fe91115c50d493ae9516b9ff6b288a7409a9367ab72d5ce102, value: 843b9aca00
+		// keccak(a6eef7e35abe7026729641147f7915573c7e97b47efa546f5f6e3230263bcb49)
+		key: 0eb5be412f275a18f6e4d622aee4ff40b21467c926224771b782d4c095d1444b, value: 12
+		// keccak(8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2b)
+		key: 0f330b5986b4b5cd78d97c486641eccc150d851a200723accc40f7788f396019, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d6)
+		key: 181863daaf01f6a2e46f43a4bd0a30f4bf514c424d89a6e8d267c22ed2e4144d, value: 8307e626
+		// keccak(c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87eee)
+		key: 198a76745cf3cf8e13b3d6175ffeca2d499ef48a12b1ea4dd717fd23cc0c861a, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a35)
+		key: 1de112f3326e683c6f3f6348c31554334288aac2d7c08460794567a96d874821, value: 8307e57f
+		// keccak(d655dac6cd810cc6fa88e194418d5d68ee3c0d1524ca99102d7454b48a45fcc8)
+		key: 25d01797e35f287ab7b68bfa28daff9bacd35d7a3a11bbd86d346df0c75fc8d4, value: 12
+		// keccak(0000000000000000000000000000000000000000000000000000000000000000)
+		key: 290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563, value: 9501ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d2)
+		key: 297b7330606da1b3a511cb74d59067f686371c2dfcf08c40f28d4ccae4bbb8eb, value: 94cc74039c82c300ba9b7d348470df2972a34d2601
+		// keccak(7ff1ccd6c3a4ffcca1b442665f88379b09ebea17412d175160ef108fe4d27708)
+		key: 2f33a69a87216483b4520801f90de950df9b32cd7c5857c73cbcb6483944c139, value: 12
+		// keccak(bfd358e93f18da3ed276c3afdbdba00b8f0b6008a03476a6a86bd6320ee6938d)
+		key: 396165d6992c35a716a6bc9d98532b3f528646f1a3e24db50abaab1fdca4bbf6, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206511)
+		key: 41b840011203114f159c752197e2f8f7da89be45d8a75b8bc1c151480a801665, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d4)
+		key: 5212dd3fe5e3427c96b9ec571f57bae61f5045df576715e6ad5c2e47454a4090, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d3)
+		key: 53960299ca52c12b1d83572f59016ab426eefe84c8aca873d5cb81d6166aef9e, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206510)
+		key: 5632bc4de74c8f7366d160f9d99e3f33c45c7046b2ddb33c49c80bceccef7424, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(bfd358e93f18da3ed276c3afdbdba00b8f0b6008a03476a6a86bd6320ee6938c)
+		key: 652fc59a8f2a19f7ee5cc6b2d4dd78e164ba7427a79f796a06232d0c74930eba, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87ef0)
+		key: 7250b5ed9b2cc3ba67419e9f0f3fcf6df0c8740fb3ea407b800dff28fc7050d7, value: 843b9aca00
+		// keccak(c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87ef1)
+		key: 73836cef606e2ad6102a1461183e38562bb425865f2bc53a67e615a52b4078cd, value: 8308b9d6
+		// keccak(697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f20650f)
+		key: 83010eb9820c88e824885fd8f7f32424ad835133ddc769cedc18104b33953abf, value: 94cc74039c82c300ba9b7d348470df2972a34d2601
+		// keccak(75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d5)
+		key: 8317c5425748c10e50f148c5ce1da7b8a582e560dd93bc2f5b7cf9bf5ff24a77, value: 843b9aca00
+		// keccak(0000000000000000000000000000000000000000000000000000000000000004)
+		key: 8a35acfbc15ff81a39ae7d344fd709f28e8600b4aa8c65c6b64bfe7fe36bd19b, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87eed)
+		key: a24bfc18fcc1e244ddb0876050d57bb7afc7d3015357149890d41dc0983789d7, value: 94cc74039c82c300ba9b7d348470df2972a34d2601
+		// keccak(8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2a)
+		key: a92cdd2c356768eae1dec11953ac097554e9b37aff77f3e27e48327ae70b5d8e, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a34)
+		key: b9a1a659fa3de49828c24b8688647f39a98f15b762105bc7dcf32d9c459340ca, value: 880de0b6b3a7640000
+		// keccak(3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a32)
+		key: cf0c9f81324142c14384a63967ad4cf1d9cebe2d8870d44219174f98a2cae008, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206513)
+		key: cf875a7874bb7bbe91aea7f1b7d40ebfc3472ec92f5f617a084432e4af3e7c79, value: 8308b9fd
+		// keccak(bfd358e93f18da3ed276c3afdbdba00b8f0b6008a03476a6a86bd6320ee6938f)
+		key: d14bd34b88c580425bfde9f3cda36813aaf1b78ba7d50fd94249991ecfc12969, value: 8308b9d9
+		// keccak(8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2c)
+		key: d6aea97326ba2dea31ead03266fdf408ffe1cd658f3a1437b19f15b2a33790e7, value: 843b9aca00
+		// keccak(8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2d)
+		key: ecebce584bb8d44beca09dcc6bac05c72b516102f58e9dffd18557b2f2df808d, value: 8307e5f1
+		// keccak(c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87eef)
+		key: eece3f48c03ffd0d090e30e6fc0dfc7b45019d71a75fafafccc76577ce178f2d, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+		// keccak(8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace29)
+		key: f6c9906f00400cd8011256368849abf5d045cb493cf4841c92ea0271eae73bbd, value: 94cc74039c82c300ba9b7d348470df2972a34d2601
+		// keccak(3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a33)
+		key: ff2898466379f431e1a96c0e04780b196f7d4528c6cc7050e97ef25b10b39d97, value: 94ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95
+	*/
+	/*
+		From FlatTrie log
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba0000000000000000000000000000000000000000000000000000000000000000, v: 01ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 517464
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba0000000000000000000000000000000000000000000000000000000000000004, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 517464
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba0000000000000000000000000000000000000000000000000000000000000005, v: 06, step: 571901
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a32, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 517503
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a33, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 517503
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a34, v: 0de0b6b3a7640000, step: 517503
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a35, v: 07e57f, step: 517503
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f20650f, v: cc74039c82c300ba9b7d348470df2972a34d2601, step: 571901
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206510, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 571901
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206511, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 571901
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206512, v: 3b9aca00, step: 571901
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206513, v: 08b9fd, step: 571901
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d2, v: cc74039c82c300ba9b7d348470df2972a34d2601, step: 517670
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d3, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 517670
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d4, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 517670
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d5, v: 3b9aca00, step: 517670
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d6, v: 07e626, step: 517670
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba7ff1ccd6c3a4ffcca1b442665f88379b09ebea17412d175160ef108fe4d27708, v: 12, step: 517478
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace29, v: cc74039c82c300ba9b7d348470df2972a34d2601, step: 517617
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2a, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 517617
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2b, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 517617
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2c, v: 3b9aca00, step: 517617
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0ba8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2d, v: 07e5f1, step: 517617
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0baa6eef7e35abe7026729641147f7915573c7e97b47efa546f5f6e3230263bcb49, v: 12, step: 517464
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0babfd358e93f18da3ed276c3afdbdba00b8f0b6008a03476a6a86bd6320ee6938c, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 571865
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0babfd358e93f18da3ed276c3afdbdba00b8f0b6008a03476a6a86bd6320ee6938d, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 571865
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0babfd358e93f18da3ed276c3afdbdba00b8f0b6008a03476a6a86bd6320ee6938f, v: 08b9d9, step: 571865
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0bac5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87eed, v: cc74039c82c300ba9b7d348470df2972a34d2601, step: 571862
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0bac5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87eee, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 571862
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0bac5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87eef, v: ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95, step: 571862
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0bac5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87ef0, v: 3b9aca00, step: 571862
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0bac5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87ef1, v: 08b9d6, step: 571862
+			storage: addr: <nil>, k: fb390fe9eacaaf75d88bc8ce82038219da25a0bad655dac6cd810cc6fa88e194418d5d68ee3c0d1524ca99102d7454b48a45fcc8, v: 12, step: 517614
+	*/
+	storageUpdates := []struct {
+		slot  []byte
+		value []byte
+	}{
+		// 571905
+		{common.Hex2Bytes("0000000000000000000000000000000000000000000000000000000000000000"), common.Hex2Bytes("01ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("0000000000000000000000000000000000000000000000000000000000000004"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("0000000000000000000000000000000000000000000000000000000000000005"), common.Hex2Bytes("06")},
+		{common.Hex2Bytes("3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a32"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a33"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a34"), common.Hex2Bytes("0de0b6b3a7640000")},
+		{common.Hex2Bytes("3e5fec24aa4dc4e5aee2e025e51e1392c72a2500577559fae9665c6d52bd6a35"), common.Hex2Bytes("07e57f")},
+		{common.Hex2Bytes("697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f20650f"), common.Hex2Bytes("cc74039c82c300ba9b7d348470df2972a34d2601")},
+		{common.Hex2Bytes("697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206510"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206511"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206512"), common.Hex2Bytes("3b9aca00")},
+		{common.Hex2Bytes("697b2bd7bb2984c4e0dc14c79c987d37818484a62958b9c45a0e8b962f206513"), common.Hex2Bytes("08b9fd")},
+		{common.Hex2Bytes("75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d2"), common.Hex2Bytes("cc74039c82c300ba9b7d348470df2972a34d2601")},
+		{common.Hex2Bytes("75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d3"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d4"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d5"), common.Hex2Bytes("3b9aca00")},
+		{common.Hex2Bytes("75f96ab15d697e93042dc45b5c896c4b27e89bb6eaf39475c5c371cb2513f7d6"), common.Hex2Bytes("07e626")},
+		{common.Hex2Bytes("7ff1ccd6c3a4ffcca1b442665f88379b09ebea17412d175160ef108fe4d27708"), common.Hex2Bytes("12")},
+		{common.Hex2Bytes("8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace29"), common.Hex2Bytes("cc74039c82c300ba9b7d348470df2972a34d2601")},
+		{common.Hex2Bytes("8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2a"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2b"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2c"), common.Hex2Bytes("3b9aca00")},
+		{common.Hex2Bytes("8819ef417987f8ae7a81f42cdfb18815282fe989326fbff903d13cf0e03ace2d"), common.Hex2Bytes("07e5f1")},
+		{common.Hex2Bytes("a6eef7e35abe7026729641147f7915573c7e97b47efa546f5f6e3230263bcb49"), common.Hex2Bytes("12")},
+		{common.Hex2Bytes("bfd358e93f18da3ed276c3afdbdba00b8f0b6008a03476a6a86bd6320ee6938c"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("bfd358e93f18da3ed276c3afdbdba00b8f0b6008a03476a6a86bd6320ee6938d"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("bfd358e93f18da3ed276c3afdbdba00b8f0b6008a03476a6a86bd6320ee6938f"), common.Hex2Bytes("08b9d9")},
+		{common.Hex2Bytes("c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87eed"), common.Hex2Bytes("cc74039c82c300ba9b7d348470df2972a34d2601")},
+		{common.Hex2Bytes("c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87eee"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87eef"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87ef0"), common.Hex2Bytes("3b9aca00")},
+		{common.Hex2Bytes("c5069e24aaadb2addc3e52e868fcf3f4f8acf5a87e24300992fd4540c2a87ef1"), common.Hex2Bytes("08b9d6")},
+		{common.Hex2Bytes("d655dac6cd810cc6fa88e194418d5d68ee3c0d1524ca99102d7454b48a45fcc8"), common.Hex2Bytes("12")},
+		// 571906
+		{common.Hex2Bytes("0000000000000000000000000000000000000000000000000000000000000005"), common.Hex2Bytes("07")},
+		{common.Hex2Bytes("4ced6d0d36392b04cc5d8761b1327b3bbba6e1089c77f60a9a9ca18e05e4f00f"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("4ced6d0d36392b04cc5d8761b1327b3bbba6e1089c77f60a9a9ca18e05e4f010"), common.Hex2Bytes("ec0e8ff20c7ec07d8f0970edbbfb745c4c32bc95")},
+		{common.Hex2Bytes("4ced6d0d36392b04cc5d8761b1327b3bbba6e1089c77f60a9a9ca18e05e4f012"), common.Hex2Bytes("08ba02")},
+	}
+
+	stepSize := 33
+
+	st, err := NewSecureTrie(common.Hash{}, NewDatabase(dbm), &TrieOpts{})
+	if err != nil {
+		t.Fatalf("Failed to create secure trie: %v", err)
+	}
+
+	for i, update := range storageUpdates {
+		if i == stepSize {
+			break
+		}
+		enc, err := rlp.EncodeToBytes(update.value)
+		if err != nil {
+			t.Fatalf("Failed to rlp encode: %v", err)
+		}
+		st.TryUpdate(update.slot, enc)
+	}
+
+	root := st.Hash()
+	fmt.Printf("root: %x\n", root) // 0xf1ef081ee354c3d1ff6ec6188c3dd3c181553fb049b2807d3bf9a22d7dbd8487
+
+	for i, update := range storageUpdates {
+		if i < stepSize {
+			continue
+		}
+		enc, err := rlp.EncodeToBytes(update.value)
+		if err != nil {
+			t.Fatalf("Failed to rlp encode: %v", err)
+		}
+		st.TryUpdate(update.slot, enc)
+	}
+
+	fmt.Printf("root: %x\n", st.Hash()) // 0xec806ef0e7737299d0eeb72658cfacc68952e91996eb03f2a8f32b6affa137ac
+
+	f, err := NewFlatTrieWithDBManager(common.Hash{}, dbm, &addr, &TrieOpts{})
+	if err != nil {
+		t.Fatalf("Failed to create flat trie: %v", err)
+	}
+
+	for _, update := range storageUpdates {
+		f.TryUpdate(update.slot, update.value)
+	}
+
+	root2 := f.Hash()
+
+	fmt.Printf("root2: %x\n", root2)
+
+	t.Fail()
+
+}
